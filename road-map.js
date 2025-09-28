56271532ca7f1bbeee7dd87233757fda9c97637b
VU LE BAO LONG <87232600+vulebaolong@users.noreply.github.com>
2025-09-28 07:11:27 +0700

    Buổi 1

---
+++ road-map.js
@@ -0,0 +1,33 @@
+const roadMap = {
+    MySQL: {
+        "Buổi 1": "Khải giảng",
+        "Buổi 2": "Basic CRUD Database, Table",
+        "Buổi 3": "Query SQL, Relationship",
+    },
+    ExpressJS: {
+        "Buổi 4": ["- Làm quen với ExpressJS", "- MySQL2, Kết nối BE với DB", "- Kết nối FE - BE - DB", "- CORS"],
+        "Buổi 5": ["- ORM Sequelize", "- Code First, Database First", "- ORM Prisma"],
+        "Buổi 6": ["- Áp dụng mô hình MVC"],
+        "Buổi 7": ["- Xử lý lỗi"],
+    },
+    Auth: {
+        "Buổi 8": ["- Xây dựng Api Đăng nhập, Đăng ký", "- Mã hoá password", "- Login Google, Demo Hack Facebook"],
+        "Buổi 9": ["- Gửi email", "- Cấu Trúc Token", "- Access Token (jsonwebtoken)"],
+        "Buỏi 10": ["- Refresh Token (jsonwebtoken)", "- Middleware bảo vệ API", "- Middleware validate"],
+        "Buổi 11": ["- Phần Quyền", "- Role, Permission"],
+        "Buổi 12": ["- Viết unit test với Jest", "- Debug trên vscode", "- Swagger API"],
+    },
+    Prisma: {
+        "Buổi 13": ["- Xây dựng API get-info", "- Chức năng đăng nhập 2FA"],
+        "Buổi 14": ["- GraphQL", "- Upload local, Upload Cloud", "- Ráp API FE"],
+        "Buổi 15": ["- Socket IO", "- Xây dựng chức năng chat"],
+    },
+    NestJS: {
+        "Buổi 16": ["- Cấu trúc module", "- Giao tiếp giữa các module", "- Decorator"],
+        "Buổi 17": ["- Xử lý Token với passport, strategy"],
+    },
+    Docker: {
+        "Buổi 18": ["- Tìm hiểu Docker", "- Tìm hiểu docker-compose"],
+        "Buổi 19": ["- Deploy hoàn chỉnh lên server", "- CI/CD"],
+    },
+};
